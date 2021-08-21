6288cd8deb0c3a4837b98640883251fa49a50856
maa123 <maa123.lq@gmail.com>
2021-08-21 16:04:25 +0900

    Update init.sh

--- go/init.sh
+++ go/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [$(hostname) == "ip-192-168-0-11"]; then
 curl -X POST -H "Content-Type: application/json" -d '{"jia_service_url":""}' https://isucondition-2.t.isucon.dev/initialize
+curl -X POST -H "Content-Type: application/json" -d '{"jia_service_url":""}' https://isucondition-3.t.isucon.dev/initialize
 else
 ../sql/init.sh
 fi
