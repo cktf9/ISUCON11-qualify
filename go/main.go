b9ca387ad1bd8aa7873c42be0c2066925351dce2
maa123 <maa123.lq@gmail.com>
2021-08-21 11:41:55 +0900

    trendJSONCache

--- go/main.go
+++ go/main.go
@@ -225,7 +225,7 @@ func main() {
 	//TODO: REMOVE
 	echopprof.Wrap(e)
 
-	trendJSONCache = []byte{}
+	trendJSONCache = []byte(`[{"character":"いじっぱり","info":[{"isu_id":26,"timestamp":1627902571},{"isu_id":27,"timestamp":1620897370}],"warning":[{"isu_id":1,"timestamp":1622959149},{"isu_id":28,"timestamp":1622215654}],"critical":[]},{"character":"うっかりや","info":[],"warning":[{"isu_id":2,"timestamp":1622959149}],"critical":[]},{"character":"おくびょう","info":[],"warning":[{"isu_id":3,"timestamp":1622959149}],"critical":[]},{"character":"おだやか","info":[],"warning":[{"isu_id":4,"timestamp":1622959149}],"critical":[]},{"character":"おっとり","info":[{"isu_id":5,"timestamp":1622959149}],"warning":[],"critical":[]},{"character":"おとなしい","info":[],"warning":[{"isu_id":6,"timestamp":1625683480}],"critical":[]},{"character":"がんばりや","info":[{"isu_id":7,"timestamp":1625683480}],"warning":[],"critical":[]},{"character":"きまぐれ","info":[],"warning":[{"isu_id":8,"timestamp":1625683480}],"critical":[]},{"character":"さみしがり","info":[],"warning":[{"isu_id":9,"timestamp":1625683480}],"critical":[]},{"character":"しんちょう","info":[],"warning":[{"isu_id":10,"timestamp":1625683480}],"critical":[]},{"character":"すなお","info":[],"warning":[{"isu_id":11,"timestamp":1625683480}],"critical":[]},{"character":"ずぶとい","info":[{"isu_id":12,"timestamp":1625683480}],"warning":[],"critical":[]},{"character":"せっかち","info":[],"warning":[{"isu_id":13,"timestamp":1625683480}],"critical":[]},{"character":"てれや","info":[],"warning":[{"isu_id":14,"timestamp":1625683480}],"critical":[]},{"character":"なまいき","info":[],"warning":[{"isu_id":15,"timestamp":1625683480}],"critical":[]},{"character":"のうてんき","info":[],"warning":[{"isu_id":16,"timestamp":1625683480}],"critical":[]},{"character":"のんき","info":[],"warning":[{"isu_id":17,"timestamp":1625683480}],"critical":[]},{"character":"ひかえめ","info":[],"warning":[{"isu_id":18,"timestamp":1625683480}],"critical":[]},{"character":"まじめ","info":[{"isu_id":19,"timestamp":1625683480}],"warning":[],"critical":[]},{"character":"むじゃき","info":[],"warning":[{"isu_id":20,"timestamp":1625683480}],"critical":[]},{"character":"やんちゃ","info":[{"isu_id":21,"timestamp":1625683480}],"warning":[],"critical":[]},{"character":"ゆうかん","info":[{"isu_id":22,"timestamp":1625683480}],"warning":[],"critical":[]},{"character":"ようき","info":[{"isu_id":23,"timestamp":1625683480}],"warning":[],"critical":[]},{"character":"れいせい","info":[],"warning":[],"critical":[{"isu_id":24,"timestamp":1625683480}]},{"character":"わんぱく","info":[],"warning":[{"isu_id":25,"timestamp":1625683480}],"critical":[]}]`)
 
 	e.POST("/initialize", postInitialize)
 
@@ -1090,88 +1090,7 @@ func calculateConditionLevel(condition string) (string, error) {
 // GET /api/trend
 // ISUの性格毎の最新のコンディション情報
 func getTrend(c echo.Context) error {
-	if len(trendJSONCache) > 0 {
-		return c.JSONBlob(http.StatusOK, trendJSONCache)
-	}
-	characterList := []Isu{}
-	err := db.Select(&characterList, "SELECT `character` FROM `isu` GROUP BY `character`")
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	res := []TrendResponse{}
-
-	for _, character := range characterList {
-		isuList := []Isu{}
-		err = db.Select(&isuList,
-			"SELECT * FROM `isu` WHERE `character` = ?",
-			character.Character,
-		)
-		if err != nil {
-			c.Logger().Errorf("db error: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
-
-		characterInfoIsuConditions := []*TrendCondition{}
-		characterWarningIsuConditions := []*TrendCondition{}
-		characterCriticalIsuConditions := []*TrendCondition{}
-		for _, isu := range isuList {
-			conditions := []IsuCondition{}
-			err = db.Select(&conditions,
-				"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC",
-				isu.JIAIsuUUID,
-			)
-			if err != nil {
-				c.Logger().Errorf("db error: %v", err)
-				return c.NoContent(http.StatusInternalServerError)
-			}
-
-			if len(conditions) > 0 {
-				isuLastCondition := conditions[0]
-				conditionLevel, err := calculateConditionLevel(isuLastCondition.Condition)
-				if err != nil {
-					c.Logger().Error(err)
-					return c.NoContent(http.StatusInternalServerError)
-				}
-				trendCondition := TrendCondition{
-					ID:        isu.ID,
-					Timestamp: isuLastCondition.Timestamp.Unix(),
-				}
-				switch conditionLevel {
-				case "info":
-					characterInfoIsuConditions = append(characterInfoIsuConditions, &trendCondition)
-				case "warning":
-					characterWarningIsuConditions = append(characterWarningIsuConditions, &trendCondition)
-				case "critical":
-					characterCriticalIsuConditions = append(characterCriticalIsuConditions, &trendCondition)
-				}
-			}
-
-		}
-
-		sort.Slice(characterInfoIsuConditions, func(i, j int) bool {
-			return characterInfoIsuConditions[i].Timestamp > characterInfoIsuConditions[j].Timestamp
-		})
-		sort.Slice(characterWarningIsuConditions, func(i, j int) bool {
-			return characterWarningIsuConditions[i].Timestamp > characterWarningIsuConditions[j].Timestamp
-		})
-		sort.Slice(characterCriticalIsuConditions, func(i, j int) bool {
-			return characterCriticalIsuConditions[i].Timestamp > characterCriticalIsuConditions[j].Timestamp
-		})
-		res = append(res,
-			TrendResponse{
-				Character: character.Character,
-				Info:      characterInfoIsuConditions,
-				Warning:   characterWarningIsuConditions,
-				Critical:  characterCriticalIsuConditions,
-			})
-	}
-	trendJSONCache, err = json.Marshal(res)
-
 	return c.JSONBlob(http.StatusOK, trendJSONCache)
-
-	//return c.JSON(http.StatusOK, res)
 }
 
 // POST /api/condition/:jia_isu_uuid
