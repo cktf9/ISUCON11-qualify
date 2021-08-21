e8448fd95c319e5e15711261b5db7d9358481f2d
maa123 <maa123.lq@gmail.com>
2021-08-21 16:34:13 +0900

    Update init.sh

--- sql/init.sh
+++ sql/init.sh
@@ -3,6 +3,7 @@ set -xu -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
 export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+export MYSQL_HOST_3="isucondition-3.t.isucon.dev"
 export MYSQL_PORT=${MYSQL_PORT:-3306}
 export MYSQL_USER=${MYSQL_USER:-isucon}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isucondition}
@@ -10,4 +11,5 @@ export MYSQL_PWD=${MYSQL_PASS:-isucon}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
+cat 0_Schema.sql 1_InitData.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME -p$MYSQL_PWD
 cat 0_Schema.sql 1_InitData.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME -p$MYSQL_PWD
