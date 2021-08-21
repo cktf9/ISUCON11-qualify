bc0314f936f08bc4a39c2e6dd480ba6b7c567441
maa123 <maa123.lq@gmail.com>
2021-08-21 10:36:47 +0900

    add pprof

--- go/main.go
+++ go/main.go
@@ -24,6 +24,9 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+
+	//test
+	"github.com/sevenNt/echo-pprof"
 )
 
 const (
@@ -213,6 +216,8 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	echopprof.Wrap(e)
 
 	e.POST("/initialize", postInitialize)
 
